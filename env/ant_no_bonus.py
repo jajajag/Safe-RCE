7db57d099f8995e769d6b991c4412cfcb941d420
Jag Zhang <zzzsgsb@gmail.com>
2022-05-23 01:21:47 -0400

    Backup.

--- env/ant_no_bonus.py
+++ env/ant_no_bonus.py
@@ -1,6 +1,7 @@
 import numpy as np
 from gym import utils
 from gym.envs.mujoco import mujoco_env
+import tqdm
 
 class AntNoBonusEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     """
@@ -59,3 +60,15 @@ class AntNoBonusEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def check_violation(self, states):
         heights = states[:,0]
         return ~(np.isfinite(states).all(axis=1) & (heights >= 0.2) & (heights <= 1.0))
+
+    def get_dataset(self, num_obs=256):
+        # This generates examples at ~145 observations / sec. When using image
+        # observations is slows down to ~17 FPS.
+        action_vec = [self.action_space.sample() for _ in range(num_obs)]
+        obs_vec = [self._get_obs() for _ in tqdm.trange(num_obs)]
+        dataset = {
+                'observations': np.array(obs_vec, dtype=np.float32),
+                'actions': np.array(action_vec, dtype=np.float32),
+                'rewards': np.zeros(num_obs, dtype=np.float32),
+        }
+        return dataset
